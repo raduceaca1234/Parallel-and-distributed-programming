63ea98a6be3a8e9ecdffcc216dca98dc93547199
raduceaca1234 <ceaca.radu@yahoo.com>
2020-11-17 17:35:15 +0200

    Added lab4

---
+++ lab4PDP/lab4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using lab4.impl;
+
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hosts = new[]
+                {
+                    "www.cs.ubbcluj.ro/~rlupsa/edu/pdp/lab-4-futures-continuations.html",
+                    "www.cs.ubbcluj.ro/~rlupsa/edu/pdp/lab-5-parallel-algo.html",
+                    "www.cs.ubbcluj.ro/~rlupsa/edu/index.html"
+                }
+                .ToList();
+             //TaskNonAsync.Run(hosts);
+            //TaskAsync.Run(hosts);
+            Callback.Run(hosts);
+
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
